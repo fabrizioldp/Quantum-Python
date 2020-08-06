c987882d6e31bf4236208274a2d01c31a7e51959
fabrizioldp <falopezdp@gmail.com>
2020-08-06 18:45:56 -0500

    reorganizando archivos

---
+++ Quantum_clases/clase_01/area_rectangulo.py
@@ -0,0 +1,5 @@
+base = 10 
+altura = 2
+area = base * altura
+print('El área del rectángulo es: ' + str(area))
+
---
+++ Quantum_clases/clase_01/contar_caracteres.py
@@ -0,0 +1,5 @@
+print('Hola. ingresa una palabra')
+palabra = input()
+total_caracteres = len(palabra)
+print('---------')
+print(total_caracteres)
